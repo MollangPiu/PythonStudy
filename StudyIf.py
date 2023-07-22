5e091b815656325b52460b772b4674f8971a7e78
MHS <hayaruby7909@gmail.com>
2023-07-23 02:19:09 +0900

    Feat: if문

---
+++ StudyIf.py
@@ -0,0 +1,23 @@
+num = 15
+num2 = 100
+num3 = 77
+str = 'tom n toms'
+coffe = 'coffe'
+
+if num < 10:
+    print('true 입니다.')
+elif num < 20:
+    print('elif 입니다.')
+else:
+    print('false 입니다.')
+
+if True:
+    print('강제 실행인가')
+else:
+    print('너는 실행되니')
+    
+if 10 < num and 90 < num2:
+    print('and 조건')
+
+if num3 < 40 or 80 < num2:
+    print('or 입니다.')
