b79b8d8f25855acb12a17da3b4bb70f1d9616dec
MHS <hayaruby7909@gmail.com>
2023-07-23 02:38:19 +0900

    Update:  if: str 비교

--- StudyIf.py
+++ StudyIf.py
@@ -3,6 +3,7 @@ num2 = 100
 num3 = 77
 str = 'tom n toms'
 coffe = 'coffe'
+strC = 'tom n toms'
 
 if num < 10:
     print('true 입니다.')
@@ -21,3 +22,8 @@ if 10 < num and 90 < num2:
 
 if num3 < 40 or 80 < num2:
     print('or 입니다.')
+
+if str == strC:
+    print('참일까')
+else:
+    print('거짓일까')
