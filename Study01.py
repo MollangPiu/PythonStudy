bb8edb033c5770f166119ba0a1ffd117b3de9b21
MHS <hayaruby7909@gmail.com>
2023-07-23 02:00:17 +0900

    Update: type Char 추가

--- Study01.py
+++ Study01.py
@@ -7,6 +7,12 @@ print(abc)
 test_ste = 'Oh'
 print(type(test_ste))
 print(test_ste)
+
+#char type 만드는 방법 알아보기
+test_char = 'a'
+print(type(test_char))
+print(test_char)
+
 testFloat = 3.14
 print(type(testFloat))
 print(testFloat)
